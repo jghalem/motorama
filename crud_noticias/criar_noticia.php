9499916b0c3920df650255f4035a48ace8ef2eb5
JoRPM <44869196+JoRPM@users.noreply.github.com>
2018-12-03 11:04:55 -0200

    adicionada preview nas noticias

---
+++ crud_noticias/criar_noticia.php
@@ -0,0 +1,18 @@
+<?php
+require_once "crud/db_connect.php";
+require_once "validar.php";
+
+echo '<form method="post" action="crud/noticias/create.php">';
+echo 'Testando</br></br>Título: <input type="text" name="titulo"></br></br>';
+echo 'Parágrafo 1: <input type="text" name="par1"></br>';
+echo 'Imagem 1: <input type="text" name="img1"></br></br>';
+echo 'Parágrafo 2: <input type="text" name="par2"></br>';
+echo 'Imagem 2: <input type="text" name="img2"></br></br>';
+echo 'Parágrafo 3: <input type="text" name="par3"></br>';
+echo 'Imagem 3: <input type="text" name="img3"></br></br>';
+echo 'Parágrafo 4: <input type="text" name="par4"></br>';
+echo 'Imagem 4: <input type="text" name="img4"></br></br>';
+echo 'Preview: <input type="text" name="preview"></br></br>';
+
+echo '<button type="submit">Enviar</button></form><a href="index.php"><button type="text">Voltar</button></a>';
+?>
---
+++ crud_noticias/noticias/update.php
@@ -0,0 +1,45 @@
+<?php
+ 
+require_once "../db_connect.php";
+
+require_once "../../validar.php";
+ 
+if($_POST) {
+    $cr_titulo = $_POST['titulo'];
+    $cr_par1 = $_POST['par1'];
+    $cr_img1 = $_POST['img1'];
+    $cr_par2 = $_POST['par2'];
+    $cr_img2 = $_POST['img2'];
+    $cr_par3 = $_POST['par3'];
+    $cr_img3 = $_POST['img3'];
+    $cr_par4 = $_POST['par4'];
+    $cr_img4 = $_POST['img4'];
+    $cr_preview = $_POST['preview'];
+    $tipo = $_SESSION['tipo_usuario'];
+
+    $id = $_GET['id'];
+    if ($tipo == "1")
+    {
+    $sql = "UPDATE noticias SET titulo = '$cr_titulo', par1 = '$cr_par1', img1 = '$cr_img1', par2 = '$cr_par2', img2 = '$cr_img2', par3 = '$cr_par3', img3 = '$cr_img3', par4 = '$cr_par4', img4 = '$cr_img4', preview = '$cr_preview' WHERE id_noticia = {$id}";
+    }
+    else
+
+    if ($tipo !== "1")
+    {
+        echo "<p>Erro: Você precisa ser um administrador para editar notícias!</p>";
+        echo "<a href='../../index.php'><button type='button'>Início</button></a>";
+        exit;
+    }
+
+    if($connect->query($sql) === TRUE) {
+        echo "<p>Notícia atualizada com sucesso!</p>";
+        echo "<a href='../../ver_noticia.php?id=".$_GET['id']."'><button type='button'>Voltar</button></a>";
+    } else {
+        echo "Erro atualizando usuário : ". $connect->error;
+    }
+ 
+    $connect->close();
+ 
+}
+ 
+?>
