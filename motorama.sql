48fbeecbc9c843daf01aed39dee74bf4d6914651
rafabra988 <44869209+rafabra988@users.noreply.github.com>
2018-12-03 11:03:19 -0200

    completo

--- motorama.sql
+++ motorama.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2018 at 07:06 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
+-- Generation Time: 03-Dez-2018 às 14:06
+-- Versão do servidor: 10.1.10-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comentarios`
+-- Estrutura da tabela `comentarios`
 --
 
 CREATE TABLE `comentarios` (
@@ -35,7 +35,7 @@ CREATE TABLE `comentarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `comentarios`
+-- Extraindo dados da tabela `comentarios`
 --
 
 INSERT INTO `comentarios` (`id_comentario`, `user_comentario`, `conteudo_comentario`, `data_comentario`, `noticia`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `comentarios` (`id_comentario`, `user_comentario`, `conteudo_comenta
 -- --------------------------------------------------------
 
 --
--- Table structure for table `noticias`
+-- Estrutura da tabela `noticias`
 --
 
 CREATE TABLE `noticias` (
@@ -60,21 +60,22 @@ CREATE TABLE `noticias` (
   `par4` varchar(1000) DEFAULT NULL,
   `img4` varchar(1000) DEFAULT NULL,
   `data` datetime DEFAULT CURRENT_TIMESTAMP,
-  `original_poster` int(11) DEFAULT NULL
+  `original_poster` int(11) DEFAULT NULL,
+  `preview` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `noticias`
+-- Extraindo dados da tabela `noticias`
 --
 
-INSERT INTO `noticias` (`id_noticia`, `titulo`, `par1`, `img1`, `par2`, `img2`, `par3`, `img3`, `par4`, `img4`, `data`, `original_poster`) VALUES
-(9, 'dwqdqw', 'dwqdqw', 'dwqdqw', '', '', '', '', '', '', '2018-12-02 15:37:56', 2),
-(10, 'teste 123', 'fabiano', 'araujo', '', '', '', '', '', '', '2018-12-02 15:49:18', 2);
+INSERT INTO `noticias` (`id_noticia`, `titulo`, `par1`, `img1`, `par2`, `img2`, `par3`, `img3`, `par4`, `img4`, `data`, `original_poster`, `preview`) VALUES
+(9, 'dwqdqw', 'dwqdqw', 'http://static.hasselblad.com/2015/04/Dolphins_1-1000x1000.jpg', '', '', '', '', '', '', '2018-12-02 15:37:56', 2, 'testando 123 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+(10, 'teste 123', 'fabiano', 'http://static.hasselblad.com/2015/04/Dolphins_1-1000x1000.jpg', '', '', '', '', '', '', '2018-12-02 15:49:18', 2, 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipo_usuario`
+-- Estrutura da tabela `tipo_usuario`
 --
 
 CREATE TABLE `tipo_usuario` (
@@ -83,7 +84,7 @@ CREATE TABLE `tipo_usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `tipo_usuario`
+-- Extraindo dados da tabela `tipo_usuario`
 --
 
 INSERT INTO `tipo_usuario` (`id_tipo_usuario`, `nome`) VALUES
@@ -93,7 +94,7 @@ INSERT INTO `tipo_usuario` (`id_tipo_usuario`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -103,16 +104,19 @@ CREATE TABLE `usuarios` (
   `senha` varchar(45) NOT NULL,
   `data_nasc` datetime DEFAULT NULL,
   `data_conta` datetime DEFAULT CURRENT_TIMESTAMP,
-  `tipo_usuario` int(11) NOT NULL
+  `tipo_usuario` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `usuarios`
+-- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nome`, `username`, `senha`, `data_nasc`, `data_conta`, `tipo_usuario`) VALUES
-(1, 'joao', 'joao', '81dc9bdb52d04dc20036dbd8313ed055', '2018-11-15 00:00:00', '2018-11-30 16:54:19', 2),
-(2, 'administrador', 'admin', '21232f297a57a5a743894a0e4a801fc3', '2018-11-15 00:00:00', '2018-11-30 20:59:30', 1);
+INSERT INTO `usuarios` (`id_usuario`, `nome`, `username`, `senha`, `data_nasc`, `data_conta`, `tipo_usuario`, `email`, `foto`) VALUES
+(1, 'joao', 'joao', '81dc9bdb52d04dc20036dbd8313ed055', '2018-11-15 00:00:00', '2018-11-30 16:54:19', 2, '', 'https://e0.365dm.com/18/06/768x432/skysports-f1-kimi-raikkonen_4348213.jpg?20180628171614'),
+(2, 'administrador', 'admin', '21232f297a57a5a743894a0e4a801fc3', '2018-11-15 00:00:00', '2018-11-30 20:59:30', 1, '', ''),
+(3, 'pedro', 'pedrin', '81dc9bdb52d04dc20036dbd8313ed055', '0000-00-00 00:00:00', '2018-12-02 16:54:33', 2, 'lola123@gmail.com', 'https://i.pinimg.com/originals/b8/59/f1/b859f1e68b68eec03d8f663c6668c088.jpg');
 
 --
 -- Indexes for dumped tables
@@ -169,26 +173,26 @@ ALTER TABLE `tipo_usuario`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `comentarios`
+-- Limitadores para a tabela `comentarios`
 --
 ALTER TABLE `comentarios`
   ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`user_comentario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`noticia`) REFERENCES `noticias` (`id_noticia`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `noticias`
+-- Limitadores para a tabela `noticias`
 --
 ALTER TABLE `noticias`
   ADD CONSTRAINT `noticias_ibfk_1` FOREIGN KEY (`original_poster`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `usuarios`
+-- Limitadores para a tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`tipo_usuario`) REFERENCES `tipo_usuario` (`id_tipo_usuario`);
